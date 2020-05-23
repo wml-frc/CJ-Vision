a7a78e8eda6df686ebd945360341ad650770c8fc
CJBuchel <connorbuchel@gmail.com>
2020-05-23 20:13:29 +0800

    Using binary system again (toolchains)

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,31 +14,31 @@ sudo apt-get install -y libv4l-dev v4l-utils
 sudo modprobe bcm2835-v4l2
 sudo apt-get install -y libatlas-base-dev gfortran
 
-# Download and setup OpenCV
-wget -nc https://github.com/opencv/opencv/archive/${OPENCV_VERSION}.zip -O opencv_source.zip
-rm -rf opencv-${OPENCV_VERSION}
-unzip opencv_source.zip
-cd opencv-${OPENCV_VERSION}
-mkdir build
-cd build
+# # Download and setup OpenCV
+# wget -nc https://github.com/opencv/opencv/archive/${OPENCV_VERSION}.zip -O opencv_source.zip
+# rm -rf opencv-${OPENCV_VERSION}
+# unzip opencv_source.zip
+# cd opencv-${OPENCV_VERSION}
+# mkdir build
+# cd build
 
-# Setup CMake in build file
-cmake -D CMAKE_BUILD_TYPE=RELEASE \
-  -D CMAKE_INSTALL_PREFIX=/usr/local \
-  -D BUILD_WITH_DEBUG_INFO=OFF \
-  -D BUILD_DOCS=OFF \
-  -D BUILD_EXAMPLES=OFF \
-  -D BUILD_TESTS=OFF \
-  -D BUILD_opencv_ts=OFF \
-  -D BUILD_PERF_TESTS=OFF \
-  -D INSTALL_C_EXAMPLES=OFF \
-  -D INSTALL_PYTHON_EXAMPLES=OFF \
-  -D ENABLE_NEON=ON \
-  -D WITH_LIBV4L=ON \
-    ../
-make -j4
-sudo make install
-sudo ldconfig
+# # Setup CMake in build file
+# cmake -D CMAKE_BUILD_TYPE=RELEASE \
+#   -D CMAKE_INSTALL_PREFIX=/usr/local \
+#   -D BUILD_WITH_DEBUG_INFO=OFF \
+#   -D BUILD_DOCS=OFF \
+#   -D BUILD_EXAMPLES=OFF \
+#   -D BUILD_TESTS=OFF \
+#   -D BUILD_opencv_ts=OFF \
+#   -D BUILD_PERF_TESTS=OFF \
+#   -D INSTALL_C_EXAMPLES=OFF \
+#   -D INSTALL_PYTHON_EXAMPLES=OFF \
+#   -D ENABLE_NEON=ON \
+#   -D WITH_LIBV4L=ON \
+#     ../
+# make -j4
+# sudo make install
+# sudo ldconfig
 
 
 # Add vision user and password
