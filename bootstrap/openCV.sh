acfc09829182b0b3220f79fed21b302242b7a928
CJBuchel <connorbuchel@gmail.com>
2020-08-25 21:25:28 +0800

    fixed [y/n] in script

--- bootstrap/openCV.sh
+++ bootstrap/openCV.sh
@@ -8,17 +8,17 @@ printf '\e[1;32m'
 printf "\nUpdating/Upgrading Machine\n"
 printf '\e[1;0m'
 
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
 
 # Install Libraries
 printf '\e[1;32m'
 printf "\nInstalling Libraries\n"
 printf '\e[0m'
-sudo apt-get install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-sudo apt-get install python3.5-dev python3-numpy libtbb2 libtbb-dev
-sudo apt-get install libjpeg-dev libpng-dev libtiff5-dev libjasper-dev libdc1394-22-dev libeigen3-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev sphinx-common libtbb-dev yasm libfaac-dev libopencore-amrnb-dev libopencore-amrwb-dev libopenexr-dev libgstreamer-plugins-base1.0-dev libavutil-dev libavfilter-dev libavresample-dev
-sudo apt-get install libopencv-dev
+sudo apt-get -y install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get -y install python3.5-dev python3-numpy libtbb2 libtbb-dev
+sudo apt-get -y install libjpeg-dev libpng-dev libtiff5-dev libjasper-dev libdc1394-22-dev libeigen3-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev sphinx-common libtbb-dev yasm libfaac-dev libopencore-amrnb-dev libopencore-amrwb-dev libopenexr-dev libgstreamer-plugins-base1.0-dev libavutil-dev libavfilter-dev libavresample-dev
+sudo apt-get -y install libopencv-dev
 
 # Get opencv
 printf '\e[1;32m'
