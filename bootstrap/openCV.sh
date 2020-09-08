dc030daf8271df80aeb94a2867d6f296a944bb60
Connor J Buchel <connorbuchel@gmail.com>
2020-09-08 21:29:43 +0800

    Update openCV.sh

--- bootstrap/openCV.sh
+++ bootstrap/openCV.sh
@@ -36,7 +36,7 @@ cd opencv
 sudo mkdir release
 cd release
 sudo cmake -D BUILD_TIFF=ON -D WITH_CUDA=OFF -D ENABLE_AVX=OFF -D WITH_OPENGL=OFF -D WITH_OPENCL=OFF -D WITH_IPP=OFF -D WITH_TBB=ON -D BUILD_TBB=ON -D WITH_EIGEN=OFF -D WITH_V4L=OFF -D WITH_VTK=OFF -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D OPENCV_EXTRA_MODULES_PATH=/opt/opencv_contrib/modules /opt/opencv/
-sudo make -j12
+sudo make -j4
 sudo make install
 sudo ldconfig
 
