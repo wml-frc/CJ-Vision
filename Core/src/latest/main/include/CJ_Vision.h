ce6891cc25c2aeb47ffa11d4041501940b137820
CJBuchel <connorbuchel@gmail.com>
2021-09-18 00:22:03 +0800

    Stared work on image container

--- Core/src/latest/main/include/CJ_Vision.h
+++ Core/src/latest/main/include/CJ_Vision.h
@@ -1,4 +1,9 @@
 #ifndef CJ_VISION_H
 #define CJ_VISION_H
+
+// Common
 #include "Application.h"
+
+// Vision
+#include "ImageContainer.h"
 #endif // CJ_VISION_HB
--- Core/src/latest/main/include/ImageContainer.h
+++ Core/src/latest/main/include/ImageContainer.h
@@ -5,7 +5,20 @@
 
 namespace CJ {
 	struct Image {
-		
+		/**
+		 * Data
+		 */
+		cv::Mat data; // Main image data
+		std::string name = "Image"; // Name
+
+		/**
+		 * Operators
+		 */
+
+		// mat operator
+		operator cv::Mat() {
+			return data;
+		}
 	};
 }
 
